b4e24bddf796d78db7023f2868cdc57baf11ca21
ChristianIQ <irizarry.christian@gmail.com>
2024-11-04 15:32:51 -0400

    cambios en supabase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY; // Asegúrate de que coincida con el nombre en .env
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
+// Agrega los console.log aquí para verificar que las variables no estén undefined
+console.log("Supabase URL:", supabaseUrl);
+console.log("Supabase Key:", supabaseKey);
+
+// Crea el cliente de Supabase
 export const supabase = createClient(supabaseUrl, supabaseKey);
